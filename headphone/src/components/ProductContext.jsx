710e10a949a704d6500e53de877d4dfa4533410f
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-02-04 17:56:57 +0100

    localstorage

--- headphone/src/components/ProductContext.jsx
+++ headphone/src/components/ProductContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
 // eslint-disable-next-line react-refresh/only-export-components
 export const ProductContext = createContext();
 
 export function ProductProvider({ children }) {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(() => {
+    const savedProducts = localStorage.getItem("products");
+    return savedProducts ? JSON.parse(savedProducts) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
   return (
     <ProductContext.Provider value={{ products, setProducts }}>
